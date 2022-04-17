f0ba2341a9206d9b15843f9015ad20095a0b7975
navGit-Hub <83656524+navGit-Hub@users.noreply.github.com>
2022-04-17 13:59:15 +0530

    initial-commit

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const {CustomAPIError,
+BadRequest,
+UnauthenticatedError
+} = require('../errors')
+
+
+const authMiddleware=async(req,res,next)=>{
+const authHeader=req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith(''))
+{
+    throw new UnauthenticatedError('No token provided');
+}
+const token=authHeader.split(' ')[1];
+try {
+    const decoded=jwt.verify(token,process.env.JWT_SECRET);
+ const {id,username}=decoded;
+ req.user={id,username};
+ next();
+
+} catch (error) {
+    throw new UnauthenticatedError('No access grated');
+}
+
+}
+module.exports=authMiddleware;
